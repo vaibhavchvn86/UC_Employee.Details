b1f26e6ede1ca938f662ce54816309525abb35f0
Vaibhav Chavan <vaibhavchvn86@gmail.com>
2021-09-13 16:47:27 +0530

    UC11 company Emp wage

---
+++ UCLast_FinalSolution/CompanyEmpWage.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day4.UCLast_FinalSolution
+{
+    class CompanyEmpWage
+    {
+
+        public string company;
+        public int empRatePerHour;
+        public int numOfWorkingDays;
+        public int maxHoursPerMonth;
+        public int totalEmpWage;
+
+        public CompanyEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
+        {
+            this.company = company;
+            this.empRatePerHour = empRatePerHour;
+            this.numOfWorkingDays = numOfWorkingDays;
+            this.maxHoursPerMonth = maxHoursPerMonth;
+            this.totalEmpWage = 0;
+        }
+
+        public void setTotalEmpWage(int totalEmpWage)
+        {
+            this.totalEmpWage = totalEmpWage;
+        }
+
+        public string toString()
+        {
+            return "Total Emp Wage for Company: " + this.company + " is: " + this.totalEmpWage;
+        }
+
+    }
+}
